90e8a39b717de8fdbbb344c30a44060810584a31
zack337 <zacktnoel@gmail.com>
2025-01-13 22:06:21 -0800

    Made player not auto flip right

--- capstone4/player.gd
+++ capstone4/player.gd
@@ -46,13 +46,7 @@ func _process(delta):
 	position = position.clamp(Vector2.ZERO, screen_size)
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "Walk"
-		$AnimatedSprite2D.flip_v = false
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	elif velocity.y != 0:
-			$AnimatedSprite2D.animation = "up"
-			$AnimatedSprite2D.flip_v = velocity.y > 0
-			
-	if velocity.x < 0:
-		$AnimatedSprite2D.flip_h = true
-	else:
-		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
